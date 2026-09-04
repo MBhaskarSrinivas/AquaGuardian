ac69c704d9b047923952ff494365ff7de8bc424e
MBhaskarSrinivas <bhaskarsrinivas2002@gmail.com>
2026-09-04 06:03:43 +0530

    Initial upload - AquaGuardian project

---
+++ block.h
@@ -0,0 +1,17 @@
+#include"types.h"
+extern s8 WEEK[][4];
+extern int T,TAR,L,M;
+extern s32 S;
+extern u32 LAST_TIME;
+extern  int Remainder;
+extern int p;
+extern s32 h,m,sec;
+extern volatile u32 flag;
+extern int EDIT_KEY;
+extern int D_HOUR;
+extern int D_MIN; 
+extern int D_SEC;
+extern unsigned long int DURATION;
+extern u32 CUR_T,ELP_T;
+extern volatile u32 flag;
+// u32 DB_FLAG = 0;
