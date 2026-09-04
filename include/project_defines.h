d0ebcacb8ce10f1ba91c49a317149a183f013466
MBhaskarSrinivas <bhaskarsrinivas2002@gmail.com>
2026-09-04 06:23:50 +0530

    Add AquaGuardian header files

---
+++ include/project_defines.h
@@ -0,0 +1,8 @@
+#define	EINTO_VIC_CHANO	14
+#define	EINTO_PIN_FUNC	0x0000000C	
+#define BUZZER 21
+#define YL  4
+#define GL  22
+#define RL	23 
+#define DB	20
+#define IS	1
---
+++ include/types.h
@@ -0,0 +1,8 @@
+//types.h
+typedef unsigned int u32;
+typedef unsigned char u8;
+typedef char s8;
+typedef int s32;
+typedef float f32;
+typedef short int s16;
+typedef unsigned short int u16;
